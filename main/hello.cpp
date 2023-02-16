43800c0947534a83f50ed48cb1952a930067885f
sanjaysagarad05 <114395101+sanjaysagarad05@users.noreply.github.com>
2023-02-16 14:43:46 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hi, World\n";
+  cout << "how r u , Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
